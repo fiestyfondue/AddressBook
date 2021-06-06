2ae8c3a4609163e9734441682b938e4dd195caba
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-06-06 19:56:11 +0530

    [Harsh]Added:-New classes Addressbook and contact is added

---
+++ Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HomeAddress { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string GetInfo()
+        {
+            string Message ="First Name: {0} \n Last Name: {1}\n Phone Number: {2}\nHome Address: {3}\n Email Address: {4}\n ";
+            return string.Format(Message, FirstName, LastName, PhoneNumber, HomeAddress, EmailAddress);
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook(50);
+            Console.WriteLine("Address Book Options: \n" +
+                                 "Add-to add new Entry .\n" +
+                                 "search-To search Entry. \n" +
+                                 "clear- To clear screen. \n" +
+                                 "options - Display Instructions\n");
+            while(true)
+            {
+                Console.WriteLine("Options: ");
+                string Instructions = Console.ReadLine();
+
+                if(Instructions.ToLower()=="quit")
+                {
+                    Console.Clear();
+                    Console.WriteLine("GoodBye!");
+                    break;
+                }
+                else if(Instructions.ToLower()=="Add")
+                {
+                    Console.Clear();
+                    addressBook.NewEntry();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+        }
+    }
+}
